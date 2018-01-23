0398f4f4c3694c2637f1b984dc30b67f657927a9
wangxu <wang.xu_cd133@immomo.com>
2018-01-23 21:31:25 +0800

    update

--- wx_roadmap/index.js
+++ wx_roadmap/index.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var eh = require("express-handlebars");
+var bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var moments = require("./moments");
 
 handlebars = eh.create({
@@ -27,12 +30,53 @@ app.set("view engine", "handlebars");
 app.set("port", 3000);
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cookieParser('session'));
+app.use(session({
+    secret: 'session',//与cookieParser中的一致
+    resave: true,
+    saveUninitialized:true
+}));
+
 app.get("/", function(req, res) {
     var articles = moments.getArticles();
-    res.render("home", {articles:articles});
+    res.render("home", {
+        articles:articles,
+    });
 });
 app.get("/admin_wx", function(req, res) {
-    res.render("admin_wx");
+    console.log(req.session.user);
+    if(req.session.user) {
+        res.render("admin_wx");
+    } else {
+        res.redirect("/login_wx");
+    }
+});
+app.get("/login_wx", function(req, res) {
+    res.render("login_wx");
+});
+app.get("/post", function(req, res) {
+    res.render("post_m", {
+        layout:null
+    });
+})
+app.post("/signin", function(req, res) {
+    console.log("req ::::: " + JSON.stringify(req.body));
+    if(req.body.username == "wangxu" && req.body.password == "123456") {
+        //res.header("Access-Control-Allow-Credentials", "true");
+        var user={
+           logined:1
+        }
+        req.session.user=user;
+        req.session.save((err)=>{
+            console.log("err : " + err);
+            res.redirect("/");        
+        });
+    } else {
+        res.status(400);
+        res.end("mistake");
+    }
 });
 app.use(function(req, res, next) {
     res.status(404);
